346fea5b5d811f3febb4abca24dc00327f5c65bf
Mélanie Bethermat <melaniebethermat@gmail.com>
2024-12-19 09:44:29 +0100

    `Correction route index`

--- index.php
+++ index.php
@@ -24,7 +24,6 @@ if ($route[0] === '' || $route[0] === 'accueil') {
     $empruntsEnCours = $transactionModel->countEmprunts();
     $retoursEffectues = $transactionModel->countRetours();
     $livresEnRetard = $transactionModel->countLivresEnRetard();
-    // $messagesNonLus = $transactionModel->countMessagesNonLus();
 
     if (isset($_SESSION['role']) && ($_SESSION['role'] === 'responsable' || $_SESSION['role'] === 'responsable_site')) {
         require 'app/models/Employe.php';
@@ -33,41 +32,62 @@ if ($route[0] === '' || $route[0] === 'accueil') {
     } else {
         include 'app/views/accueil.php'; // Page d'accueil pour les bibliothécaires
     }
+} elseif ($route[0] === 'accueil_admin') {
+    // Page spécifique pour les administrateurs
+    if (isset($_SESSION['role']) && ($_SESSION['role'] === 'responsable' || $_SESSION['role'] === 'responsable_site')) {
+        require 'app/models/Employe.php';
+        require 'app/controllers/EmployeController.php';
+        include 'app/views/accueil_admin.php';
+    } else {
+        header('Location: accueil');
+        exit;
+    }
 } elseif ($route[0] === 'emprunts') {
     require_once 'app/controllers/EmpruntsController.php';
     $empruntsController = new EmpruntsController($pdo);
     $empruntsController->index();
-    
-} elseif ($route[0] === 'scanner') {
-    // Gestion du scanner
-    require 'app/models/Scanner.php';
-    require 'app/controllers/ScannerController.php';
-    $scannerModel = new Scanner($pdo);
-
-    if (isset($route[1]) && $route[1] === 'resultat') {
-        include 'app/views/scanner/resultat.php';
-    } elseif (isset($route[1]) && $route[1] === 'emprunter') {
-        include 'app/views/scanner/emprunter.php';
-    } else {
-        include 'app/views/scanner/scan.php';
-    }
 } elseif ($route[0] === 'retours') {
-    // Gestion des retours
     require_once 'app/controllers/RetoursController.php';
     $retoursController = new RetoursController();
     include 'app/views/livres/retours.php';
 } elseif ($route[0] === 'retards') {
-    // Gestion des retards
     require_once 'app/controllers/RetardsController.php';
     $retardsController = new RetardsController();
     include 'app/views/livres/retards.php';
-// } elseif ($route[0] === 'messages') {
-//     // Gestion des messages
-//     require_once 'app/models/Message.php';
-//     require_once 'app/controllers/MessageController.php';
-//     $messageModel = new Message($pdo);
-//     $messageController = new MessageController($messageModel);
-//     $messageController->afficherMessages();
+} elseif ($route[0] === 'scanner') {
+    // Gestion du scanner
+    require_once 'app/models/Scanner.php';
+    require_once 'app/controllers/ScannerController.php';
+    $scannerModel = new Scanner($pdo);
+
+    if (isset($route[1]) && $route[1] === 'resultat') {
+        include 'app/views/scanner/resultat.php';
+    } elseif ($route[1] === 'emprunter') {
+        include 'app/views/scanner/emprunter.php';
+    } else {
+        include 'app/views/scanner/scan.php';
+    }
+} elseif ($route[0] === 'livres') {
+    // Gestion des livres
+    require_once 'app/controllers/LivreController.php';
+    $livreController = new LivreController();
+
+    if (isset($route[1]) && $route[1] === 'list') {
+        $livreController->listLivres();
+    } elseif ($route[1] === 'create') {
+        $livreController->createLivre();
+    } elseif ($route[1] === 'detail') {
+        $livreController->detailLivre();
+    } else {
+        include 'app/views/404.php';
+    }
+} elseif ($route[0] === 'messages') {
+    // Gestion des messages
+    require_once 'app/models/Message.php';
+    require_once 'app/controllers/MessageController.php';
+    $messageModel = new Message($pdo);
+    $messageController = new MessageController($messageModel);
+    $messageController->afficherMessages();
 } elseif ($route[0] === 'connexion') {
     // Connexion
     require_once 'app/models/Employe.php';
